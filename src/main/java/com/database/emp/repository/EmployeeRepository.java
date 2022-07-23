835c38db5d72d15f924c43536bf3153d4a72cddf
Madan Gangwar <madanji.official@gmail.com>
2022-07-23 21:41:02 +0530

    added

---
+++ src/main/java/com/database/emp/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.database.emp.repository;
+
+import com.database.emp.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+
+}
