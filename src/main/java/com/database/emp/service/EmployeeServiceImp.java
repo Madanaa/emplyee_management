4998f364087ec9af40a4a5375fc482953f09e5db
Madan Gangwar <madanji.official@gmail.com>
2022-08-09 23:02:45 +0530

    added

--- src/main/java/com/database/emp/service/EmployeeServiceImp.java
+++ src/main/java/com/database/emp/service/EmployeeServiceImp.java
@@ -3,9 +3,10 @@ package com.database.emp.service;
 import com.database.emp.model.Employee;
 import com.database.emp.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class EmployeeServiceImp implements EmployeeService{
     @Autowired
     private EmployeeRepository employeeRepository;
